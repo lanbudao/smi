815926144f8096a8b70d77d44d8f8eb409550135
lance710825 <bluelance@foxmail.com>
2020-10-03 20:50:15 +0800

    add 3rdparty.cmake

---
+++ 3rdparty.cmake
@@ -0,0 +1,11 @@
+
+#FFMPEG_DIR is necessary
+set(FFMPEG_DIR E:/source/opensource/ffmpeg/build32)
+
+# If u want to add portaudio to audio output, set the PORTAUDIO_DIR
+set(PORTAUDIO_DIR E:/source/opensource/portaudio)
+
+# If u want to build the example of SDL, set the SDL_DIR
+set(SDL_DIR E:/source/opensource/sdl)
+
+set(SOUNDTOUCH_DIR E:/source/opensource/soundtouch)
