ecb9565312d9961227e828680a37fb96faf84665
lance710825 <bluelance@foxmail.com>
2020-12-19 18:32:57 +0800

    fix cmake file

--- 3rdparty.cmake
+++ 3rdparty.cmake
@@ -5,7 +5,7 @@ set(FFMPEG_DIR C:/msys64/usr/local/ffmpeg)
 # Optional below
 
 # If u want to add portaudio to audio output, set the PORTAUDIO_DIR
-set(PORTAUDIO_DIR E:/source/opensource/portaudio)
+set(PORTAUDIO_DIR "C:\\msys64\\usr\\local\\portaudio")
 
 # if u want to add subtitle support, set the FREETYPE_DIR
 set(FREETYPE_DIR "C:\\msys64\\usr\\local\\freetype")
