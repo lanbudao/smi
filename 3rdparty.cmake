ae9e64614836fc0abd1f651063402629527233a5
lanbudao <theartedly@163.com>
2023-12-13 20:18:55 +0800

    Alter the output directory.

--- 3rdparty.cmake
+++ 3rdparty.cmake
@@ -3,19 +3,19 @@
 # Default, I will find libraries from directory named 'lib' you set, not 'bin'.
 # For example, I will find 'avformat.lib' form 'C:/msys64/usr/local/ffmpeg/lib'.
 
-set(FFMPEG_DIR C:/msys64/usr/local/ffmpeg)
+set(FFMPEG_DIR "H:\\source_laptop\\opensource\\ffmpeg_x64")
 
 # Optional below
 
 # if u want to add subtitle support, set the LIBASS_DIR
-set(LIBASS_DIR "C:\\msys64\\usr\\local\\libass")
+#set(LIBASS_DIR "C:\\msys64\\usr\\local\\libass")
 
 # If u want to add portaudio to audio output, set the PORTAUDIO_DIR
-set(PORTAUDIO_DIR "C:\\msys64\\usr\\local\\portaudio")
+#set(PORTAUDIO_DIR "C:\\msys64\\usr\\local\\portaudio")
 
 # If u want to support DVD, set the LIBDVDREAD_DIR and LIBDVDNAV_DIR
-set(LIBDVDREAD_DIR "C:\\msys64\\usr\\local\\libdvdread")
-set(LIBDVDNAV_DIR "C:\\msys64\\usr\\local\\libdvdnav")
+#set(LIBDVDREAD_DIR "C:\\msys64\\usr\\local\\libdvdread")
+#set(LIBDVDNAV_DIR "C:\\msys64\\usr\\local\\libdvdnav")
 
 # If u want to build the example of SDL, set the SDL_DIR
-set(SDL_DIR H:\\source_laptop\\opensource\\SDL)
+set(SDL_DIR "H:\\source_laptop\\opensource\\SDL2")
